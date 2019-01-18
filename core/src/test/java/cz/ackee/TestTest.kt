689f930d45ba9d9af98310e49f678772510bcbcc
Georgiy Shur <georgiy.shur@ackee.cz>
2019-01-18 17:08:05 +0100

    Test unit test 1

---
+++ core/src/test/java/cz/ackee/TestTest.kt
@@ -0,0 +1,18 @@
+package cz.ackee
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * TODO add class description
+ */
+@RunWith(JUnit4::class)
+class TestTest {
+
+    @Test
+    fun testEmptyAuthToken() {
+        assertEquals(2 + 2, 5)
+    }
+}
